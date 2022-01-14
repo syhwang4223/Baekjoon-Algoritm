77449a53c66db2ad72aeb0aeb90db0514d7e5af5
syhwang4223 <syhwang4223@gmail.com>
2022-01-14 18:21:35 +0900

    Add 1912 solution

---
+++ 1912연속합/1912.py
@@ -0,0 +1,14 @@
+from sys import stdin
+
+n = int(stdin.readline())
+arr = list(map(int, stdin.readline().split()))
+
+partsum = [0 for i in range(n)]
+partsum[0] = arr[0]
+
+
+for i in range(1,n):
+    partsum[i] = max(arr[i], partsum[i-1]+arr[i])
+    print(partsum)
+
+print(max(partsum))
