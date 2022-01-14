51c8e2ab3939301091a52f8ec94a948e79a40422
syhwang4223 <syhwang4223@gmail.com>
2022-01-14 20:17:53 +0900

    Add 1463 solution

---
+++ 1463/1463.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+dp = [0 for i in range(n+1)]
+#dp[i]: 정수 i에, 주어진 연산 세 개를 사용해서 1을 만들 때 최소횟수
+
+dp[1] = 0
+for i in range(2,n+1):
+    tmp = []
+    if i%2==0:
+        tmp.append(dp[i//2])
+    if i%3==0:
+        tmp.append(dp[i//3])
+    tmp.append(dp[i-1])
+    dp[i] = min(tmp)+1
+
+print(dp[-1])    
+print(dp)
